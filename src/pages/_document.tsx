02e1e04a68f71e596ec51dff609bbe743e444680
Adam Cosman <uenify@gmail.com>
2021-12-02 03:27:00 +0600

    fix: manifest, metatags and deploy

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,15 +32,14 @@ export default class Document extends NextDocument {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#272727" />
           <meta name="msapplication-TileColor" content="#272727" />
-          <meta name="theme-color" content="#ffffff" />
 
           <meta name="keywords" content="Registrar, Nazarbayev University, Courses, Online, Tool" />
           <meta name="author" content="superhooman" />
 
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <body>
