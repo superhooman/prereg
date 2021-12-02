cf08de284f45d33890526d1c4659aba020ba1d01
Adam Cosman <uenify@gmail.com>
2021-12-02 12:03:20 +0600

    fix: meta to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,11 @@ import { LoadingOverlay } from "src/components/LoadingOverlay";
 import { NextPageContext } from "next";
 import { Request } from "express";
 
+const TITLE = 'PreReg | superhooman';
+const DESCRIPTION = 'Get your perfect schedule';
+
+const URL = 'https://prereg.superhooman.co/';
+const OPENGRAPH = `${URL}opengraph.png`;
 const THEME_SETTING = 'reg.themeSetting';
 
 const globalStyles = globalCss({
@@ -70,8 +75,37 @@ const App = ({ Component, theme }: Props) => {
       dispatch,
     }}>
       <Head>
-        <style dangerouslySetInnerHTML={{ __html: `html{background-color:${themeToColor[state.theme]};}` }} />
-        <meta name="theme-color" content={themeToColor[state.theme]} />
+          <title>{TITLE}</title>
+          <meta name="title" content={DESCRIPTION} />
+          <meta name="description" content={DESCRIPTION} />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content={URL} />
+          <meta property="og:title" content={TITLE} />
+          <meta property="og:description" content={DESCRIPTION} />
+          <meta property="og:image" content={OPENGRAPH} />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content={URL} />
+          <meta property="twitter:title" content={TITLE} />
+          <meta property="twitter:description" content={DESCRIPTION} />
+          <meta property="twitter:image" content={OPENGRAPH} />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#272727" />
+          <meta name="msapplication-TileColor" content="#272727" />
+
+          <meta name="keywords" content="Registrar, Nazarbayev University, Courses, Online, Tool" />
+          <meta name="author" content="superhooman" />
+
+          <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="theme-color" content={themeToColor[state.theme]} />
+          <style id="ssr_theme" dangerouslySetInnerHTML={{ __html: `html{background-color:${themeToColor[state.theme]};}` }} />
       </Head>
       <LoadingOverlay value={state.progres} active={state.isLoading} text="Fetching schedule..." />
       <Component />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,47 +1,11 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { getCssText } from 'src/utils/theme';
 
-const TITLE = 'PreReg | superhooman';
-const DESCRIPTION = 'Get your perfect schedule';
-
-const URL = 'https://prereg.superhooman.co/';
-const OPENGRAPH = `${URL}opengraph.png`;
-
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
-          <title>{TITLE}</title>
-          <meta name="title" content={DESCRIPTION} />
-          <meta name="description" content={DESCRIPTION} />
-
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={URL} />
-          <meta property="og:title" content={TITLE} />
-          <meta property="og:description" content={DESCRIPTION} />
-          <meta property="og:image" content={OPENGRAPH} />
-
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={URL} />
-          <meta property="twitter:title" content={TITLE} />
-          <meta property="twitter:description" content={DESCRIPTION} />
-          <meta property="twitter:image" content={OPENGRAPH} />
-
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#272727" />
-          <meta name="msapplication-TileColor" content="#272727" />
-
-          <meta name="keywords" content="Registrar, Nazarbayev University, Courses, Online, Tool" />
-          <meta name="author" content="superhooman" />
-
-          <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         </Head>
         <body>
